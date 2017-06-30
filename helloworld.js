f061efd5e262683f6efa2a9cbb94d72d43f8c881
petenguyen13 <petenguyen13@gmail.com>
2017-06-30 09:47:55 +0700

    Update helloworld.js 2

--- helloworld.js
+++ helloworld.js
@@ -3,10 +3,6 @@ function helloWorld() {
 };
 helloWorld();
 /** This is petenguyen13@gmail.com first change **/
-
-
-
-
 /** This is petenguyen13@gmail.com first good change **/
 /** This is petenguyen13@gmail.com second bad change **/
 
